6d037539ad5109d8477c7b867c407258638b53a6
Brian Uceda <monalek0622@hotmail.com>
2024-02-13 18:42:35 -0500

    flowers code

---
+++ script.js
@@ -0,0 +1,6 @@
+onload = () =>{
+  document.body.classList.remove("container");
+  // Inicia la reproducción del audio cuando la página se carga
+  const audio = document.getElementById("audioPlayer");
+  audio.play();
+};
